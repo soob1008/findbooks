e836ac80c782c9d38a3d918a98567935967232ba
soob <1008sb354@gmail.com>
2022-06-27 22:15:55 +0900

    api

--- pages/newbook.tsx
+++ pages/newbook.tsx
@@ -6,8 +6,6 @@ import ListStyle from "../styles/List.module.css";
 import key from "./api/config";
 
 const NewBooks: NextPage = (books) => {
-  console.log(books);
-
   return (
     <>
       <Title level={3}>New Book</Title>
@@ -20,7 +18,7 @@ const NewBooks: NextPage = (books) => {
 };
 
 export const getServerSideProps = async () => {
-  const bestSellerUrl = `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${key.ttbKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`;
+  const bestSellerUrl = `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttb1008sb2140001&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101`;
 
   const res = await fetch(bestSellerUrl);
   const books = await res.json();
